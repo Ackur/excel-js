de1b4ede3ebd4fa405eb5e919a529e2c414bc734
Ackur <kremenokvv@gmail.com>
2020-12-31 12:14:23 +0100

    Implement DIP

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,3 +1,4 @@
+import { Loader } from '../../components/Loader.js';
 import {$} from '../dom.js';
 import { ActiveRoute } from './ActiveRoute';
 
@@ -10,6 +11,7 @@ export class Router {
     this.$placeholder = $(selector);
     this.routes = routes;
     this.page = null
+    this.loader = Loader
 
     this.changePageHandler = this.changePageHandler.bind(this);
 
@@ -21,7 +23,7 @@ export class Router {
     this.changePageHandler();
   }
 
-  changePageHandler() {
+  async changePageHandler() {
     if (this.page) {
       this.page.destroy()
     }
@@ -34,14 +36,15 @@ export class Router {
       }
     })
 
-    this.$placeholder.clear()
+    this.$placeholder.clear().append(this.loader())
 
     if (Page) {
       this.page = new Page(ActiveRoute.param)
-      this.$placeholder.append(this.page.getRoot())
+      const root = await this.page.getRoot()
+      this.$placeholder.clear().append(root)
       this.page.afterRender()
     } else {
-      this.$placeholder.append(`404 - Ooops`)
+      this.$placeholder.clear().append(`404 - Ooops`)
     }
   }
 
--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -14,17 +14,64 @@ function storageName(param) {
   return 'excel:' + param
 }
 
+class StateProcessor {
+  constructor(client, delay = 300) {
+    this.client = client
+    this.listen = debounce(this.listen.bind(this), delay)
+  }
+
+  listen(state) {
+    this.client.save(state)
+  }
+
+  get() {
+    return this.client.get()
+  }
+}
+
+class LocalStorageClient {
+  constructor(name) {
+    this.name = storageName(name)
+  }
+
+  save(state) {
+    storage(this.name, state)
+    return Promise.resolve()
+  }
+
+  get() {
+    return Promise.resolve(storage(this.name))
+    // return new Promise(resolve => {
+    //   const state = storage(this.name)
+    //   setTimeout(() => {
+    //     resolve(state)
+    //   }, 5000);
+    // })
+  }
+}
+
 export class ExcelPage extends Page {
-  getRoot() {
-    const params = this.params ? this.params : Date.now().toString()
-    const state = storage(storageName(params));
+  constructor(params) {
+    super(params)
+
+    this.storeSub = null
+    this.processor = new StateProcessor(
+        new LocalStorageClient(this.params),
+        400
+    )
+  }
+
+  async getRoot() {
+    // const params = this.params ? this.params : Date.now().toString()
+    // const state = storage(storageName(params));
+    const state = await this.processor.get()
     const store = createStore(rootReducer, normalizeInitialState(state));
 
-    const stateListener = debounce((state) => {
-      storage(storageName(params), state);
-    }, 300);
+    // const stateListener = debounce((state) => {
+    //   storage(storageName(params), state);
+    // }, 300);
 
-    store.subscribe(stateListener);
+    this.storeSub = store.subscribe(this.processor.listen);
 
     this.excel = new Excel({
       components: [Header, Toolbar, Formula, Table],
@@ -40,5 +87,6 @@ export class ExcelPage extends Page {
 
   destroy() {
     this.excel.destroy()
+    this.storeSub.unsubscribe()
   }
 }
